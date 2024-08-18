635806d2d21e07df04606d65c1b02b4533e280b3
Katta Navya <nikhilakatta42@gmail.com>
2024-08-18 13:18:25 +0530

    First commit on project

---
+++ index.js
@@ -0,0 +1,32 @@
+const express=require("express")
+const app=express()
+const dotEnv=require('dotenv')
+const mongoose=require("mongoose")
+const vendorRoutes=require("./routes/vendorRoutes")
+const bodyParser=require("body-parser")
+const firmRoutes=require('./routes/firmRoutes')
+const firm=require('./models/Firm')
+const productRoutes=require('./routes/productRoutes')
+const path=require('path')
+
+
+const port=4000
+dotEnv.config();
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB connected successfully")
+}).catch((error)=>{
+    console.log(error)
+})
+app.use(bodyParser.json())
+app.use("/vendor",vendorRoutes)
+app.use('/firm',firmRoutes)
+app.use('/product',productRoutes)
+app.use('/uploads',express.static('uploads'))
+app.listen(port,()=>{
+    console.log(`Server started and running at ${port}`)
+})
+
+app.use('/home',(req,res)=>{
+res.send("<h1> Welcomee</h1>")
+})
